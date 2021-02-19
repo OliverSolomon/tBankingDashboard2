ec9058d79d2be2500ccbae4f8215aa49b2c8ff6c
OliverSolomon <oliversolomonwainaina@gmail.com>
2021-02-19 13:55:36 +0300

    Made it mobile responsive

---
+++ static/main.js
@@ -0,0 +1,12 @@
+// variables
+var menuBtn = document.getElementById('menuBtn');
+var menu = document.getElementById('menu');
+
+// functions
+
+function showMenu() {
+    menu.classList.toggle("showMenuOnClick")
+    console.log("Aye Aye")
+}
+
+// console.log("Aye Aye")
